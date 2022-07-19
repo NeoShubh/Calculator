6f00361772729c46b2c950ac4f52afb611c435f1
Cap <90457528+NeoShubh@users.noreply.github.com>
2022-07-19 16:34:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,72 @@
+//declare variables
+let num1 = "";
+let num2 = "";
+let operator = "";
+let total = "";
 
-let num1='';
-let num2='';
-let operator='';
-let total='';
+//handle whether user enter number or operator
 
-$(document).ready(function(){
-
-    $('button').on('click',function(e){
-        let btn=e.target.innerHTML;
-        if(btn>='0' && btn<='9'){
-            handleNumber(btn);
-        }
-        else{
-            handleOperator(btn);
-        }
-        
-    });
+$(document).ready(function () {
+  $("button").on("click", function (e) {
+    let btn = e.target.innerHTML;
+    if (btn >= "0" && btn <= "9") {
+      handleNumber(btn);
+    } else {
+      handleOperator(btn);
+    }
+  });
 });
-
-
-
-function handleNumber(num){
-    if(num1===''){
-        num1=num;
-    }
-    else{
-        num2=num;
-    }
-    displayButton(num);
-}
-
-function handleOperator(oper){
-    if(operator===''){
-        operator=oper;
-    }
-    else
-    {
-        handleTotal();
-        operator=oper;
-    }
-}
-
-function handleTotal(){
-    switch(operator){
-        case '+':total+= +num1 + +num2;
-                 displayButton(total);
-        break;
-        case '-':total+= +num1 - +num2;
-                 displayButton(total);
-        break;
-        case '/':total+= +num1 / +num2;
-                 displayButton(total);
-        break;
-        case '*':total+= +num1 * +num2;
-                 displayButton(total);
-        break;
-        
-    }
-
-    updateVariables();
-}
-
-function displayButton(btn){
-    $('.input').text(btn);
-}
-
-function updateVariables(){
-    num1+=total;
-    num2='';
-}
-
-function refresh() {
-    location.reload();
+//handle number here
+function handleNumber(num) {
+  if (num1 === "") {
+    num1 = num;
+  } else {
+    num2 = num;
   }
+  displayButton(num);
+}
+//handle operator
+function handleOperator(oper) {
+  if (operator === "") {
+    operator = oper;
+  } else {
+    handleTotal();
+    operator = oper;
+  }
+}
+//total result
+function handleTotal() {
+  switch (operator) {
+    case "+":
+      total += +num1 + +num2;
+      displayButton(total);
+      break;
+    case "-":
+      total += +num1 - +num2;
+      displayButton(total);
+      break;
+    case "/":
+      total += +num1 / +num2;
+      displayButton(total);
+      break;
+    case "*":
+      total += +num1 * +num2;
+      displayButton(total);
+      break;
+  }
+
+  updateVariables();
+}
+
+function displayButton(btn) {
+  $(".input").text(btn);
+}
+
+function updateVariables() {
+  num1 += total;
+  num2 = "";
+}
+//button to refresh the site once after the task
+function refresh() {
+  location.reload();
+}
